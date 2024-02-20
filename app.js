6e4189a55b677b416c1aa6738061f8ed733e51fb
itc-s22020 <s22020@std.it-college.ac.jp>
2024-02-20 15:43:59 +0900

    usersの処理まで

--- app.js
+++ app.js
@@ -1,41 +1,63 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from "express";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import passport from "passport";
+import authConfig from "./util/auth.js";
+import logger from "morgan";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import indexRouter from "./routes/index.js";
+import usersRouter from "./routes/users.js";
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// session
+app.use(session({
+  secret: "B02CGO2YqVlyDXbfQ7a6CX3zNLSHzLkXM0BjRqfhIoSiVxtH",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {maxAge: 60 * 60 * 1000}
+}));
+// passport
+app.use(passport.authenticate("session"));
+authConfig(passport);
+// cors
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// 404
+app.use((req, res, next) => {
+  res.status(404).json({message: "not found."});
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+/**
+ * error handler
+ * 様々な場所でエラーが発生、または発生させて
+ * こちらでまとめて対処するための関数。
+ *
+ * @type express.ErrorRequestHandler
+ */
+const errorHandler = (err, req, res, next) => {
+  // デフォルトは内部サーバーエラーとしておく。
+  let message = "Internal Server Error";
+  if (err.status === 401) {
+    // ここに来る場合は、未認証によるエラーなのでメッセージを書き換える。
+    message = "unauthenticated";
+  } else {
+    // エラーの詳細はクライアントに返さないので、ここで吐き出しておく。
+    console.error(err);
+  }
+  res.status(err.status || 500).json({message});
+};
+app.use(errorHandler);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+export default app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,99 @@
-var express = require('express');
-var router = express.Router();
+import express from "express";
+import { check, validationResult } from "express-validator";
+import passport from "passport";
+import { calcHash, generateSalt } from "../util/auth.js";
+import { PrismaClient } from "@prisma/client";
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const router = express.Router();
+const prisma = new PrismaClient();
+
+/**
+ * ログイン状態チェック
+ */
+router.get("/check", (req, res, next) => {
+  if (!req.user) {
+    const err = new Error("NG"); //"unauthenticated"
+    err.status = 401;
+    throw err;
+  }
+  res.json({
+    message: "OK",
+
+  });
 });
 
-module.exports = router;
+
+/**
+ * ユーザ認証
+ */
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (!user) {
+      return res.status(401).json({ result: "NG"});
+    }
+    req.logIn(user, async (err) => {
+        const isAdmin = user.isAdmin || false;
+        return res.status(200).json({ result: "OK", isAdmin: isAdmin });
+    });
+  })(req, res, next);
+});
+
+
+/**
+ * ユーザ新規作成
+ */
+router.post("/register", [
+    check("name").notEmpty({ignore_whitespace: true}),
+    check("password").notEmpty({ignore_whitespace: true}),
+    check("email").isEmail() // 追加:メールアドレス
+  ], async (req, res, next) => {
+    if (!validationResult(req).isEmpty()) {
+      res.status(400).json({
+        message: "NG" //"username, password, and/or email is empty or invalid"
+      });
+      return;
+    }
+    const {name, password, email} = req.body; // 追加:メールアドレス
+    const salt = generateSalt();
+    const hashed = calcHash(password, salt);
+    try {
+      await prisma.users.create({
+        data: {
+          name,
+          password: hashed,
+          salt,
+          email // 追加:メールアドレス
+        }
+      });
+      res.status(201).json({
+        message: "created"
+      });
+    } catch (e) {
+      switch (e.code) {
+        case "P2002":
+          res.status(400).json({
+            message: "NG"  //"username is already registered"
+          });
+          break;
+        default:
+          console.error(e);
+          res.status(500).json({
+            message: "NG"   //"unknown error"
+          });
+      }
+    }
+  });
+
+
+
+
+/**
+ * ユーザーのログアウト
+ */
+router.get("/logout", (req, res) => {
+  req.logout((err) => {res.status(200).json({ result: "OK" });});
+});
+
+
+
+export default router;
